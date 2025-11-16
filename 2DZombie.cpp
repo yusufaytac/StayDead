9d5a697c466dbced698881d5b55806247eff8192
yusufaytac <aytacyusuf6@gmail.com>
2025-11-16 21:57:25 +0300

    poison

--- 2DZombie.cpp
+++ 2DZombie.cpp
@@ -18,8 +18,10 @@ int main()
     WindowDimension[1] = 512;
 
     const int Velocity = 100;
-    int MedicVel = 150;
-    int Medic2Vel = 100;
+    int MedicVel = 140;
+    int Medic2Vel = 220;
+    int Medic3Vel = 190;
+    int Medic4Vel = 170;
 
     // initialize window
     InitWindow(WindowDimension[0], WindowDimension[1], "Stay Dead!");
@@ -46,12 +48,13 @@ int main()
     };
 
     
-    const int SizeOfMedic = 10;
+    const int SizeOfMedic = 1;
+    
     // medical kit texture
     Texture2D Medic = LoadTexture("textures/medicalkit.png");
     AnimData MedicData{
         {0, 0, Medic.width, Medic.height},
-        {100, 0},
+        {0, 0},
         0,
         0,
         0     
@@ -60,17 +63,36 @@ int main()
     AnimData Medic1[SizeOfMedic]{MedicData};
     for (int i = 0; i<SizeOfMedic; i++)
     {   
-        Medic1[i].Pos.x = MedicData.Pos.x;
+        Medic1[i].Pos.x = MedicData.Pos.x - 40;
         Medic1[i].Pos.y = -(i * 600);
-    }
 
+    }
     // medic 2
     AnimData Medic2[SizeOfMedic]{MedicData};
     for (int i = 0; i<SizeOfMedic; i++)
     {
-        Medic2[i].Pos.x = MedicData.Pos.x +50;
-        Medic2[i].Pos.y = -(i * 600);
+        Medic2[i].Pos.x = MedicData.Pos.x + 55;
+        Medic2[i].Pos.y = -(i * 750);
     }
+
+    // medic 3
+    AnimData Medic3[SizeOfMedic]{MedicData};
+    for (int i = 0; i<SizeOfMedic; i++)
+    {
+        Medic3[i].Pos.x = MedicData.Pos.x + 165;
+        Medic3[i].Pos.y = -(i * 900);
+    }
+    // medic 4
+    AnimData Medic4[SizeOfMedic]{MedicData};
+    for (int i = 0; i<SizeOfMedic; i++)
+    {
+        Medic4[i].Pos.x = MedicData.Pos.x + 270;
+        Medic4[i].Pos.y = -(i * 1100);
+    }
+
+    // poison texture
+    Texture2D Poison = LoadTexture("textures/poison.png");
+    AnimData PoisonData;
 
 
     
@@ -164,22 +186,52 @@ int main()
        {
             // update the position of each medic
             Medic1[i].Pos.y = Medic1[i].Pos.y + MedicVel * DeltaTime;
+            if (Medic1[i].Pos.y > WindowDimension[1])
+            {
+                Medic1[i].Pos.y = -150; // return
+            }
             DrawTexture(Medic, Medic1[i].Pos.x, Medic1[i].Pos.y, WHITE);
        }
        // update medic2
        for (int i = 0; i<SizeOfMedic; i++)
        {
             Medic2[i].Pos.y = Medic2[i].Pos.y + Medic2Vel * DeltaTime;
+            if (Medic2[i].Pos.y > WindowDimension[1])
+            {
+                Medic2[i].Pos.y = -150; // return
+            }
             DrawTexture(Medic, Medic2[i].Pos.x, Medic2[i].Pos.y, WHITE);
+       }
+       // update medic3
+       for (int i = 0; i<SizeOfMedic; i++)
+       {
+            Medic3[i].Pos.y = Medic3[i].Pos.y + Medic3Vel * DeltaTime;
+            if (Medic3[i].Pos.y > WindowDimension[1])
+            {
+                Medic3[i].Pos.y = -150; // return
+            }
+            DrawTexture(Medic, Medic3[i].Pos.x, Medic3[i].Pos.y, WHITE);
+       }
+       // update medic4
+       for (int i = 0; i<SizeOfMedic; i++)
+       {
+            Medic4[i].Pos.y = Medic4[i].Pos.y + Medic4Vel * DeltaTime;
+            if (Medic4[i].Pos.y > WindowDimension[1])
+            {
+                Medic4[i].Pos.y = -150; // return
+            }
+            DrawTexture(Medic, Medic4[i].Pos.x, Medic4[i].Pos.y, WHITE);
        }
 
 
+       //DrawTexture(Poison, 50, 50, WHITE);
 
        
       
 
        EndDrawing();
     }
+    UnloadTexture(Poison);
     UnloadTexture(Medic);
     UnloadTexture(ZombieIdle);
     UnloadTexture(ZombieWalk);
