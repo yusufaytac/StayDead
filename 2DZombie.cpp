e3957ecc8eae814a388c048557ad61764a0007a0
yusufaytac <aytacyusuf6@gmail.com>
2025-11-16 17:04:09 +0300

    character

--- 2DZombie.cpp
+++ 2DZombie.cpp
@@ -17,61 +17,120 @@ int main()
     WindowDimension[0] = 380;
     WindowDimension[1] = 512;
 
+    const int Velocity = 100;
+    
+
     // initialize window
     InitWindow(WindowDimension[0], WindowDimension[1], "Stay Dead!");
 
-    bool IsWalking = false;
-
-    // zombie idle
-    Texture2D ZombieIdle = LoadTexture("textures/Idle.png");
+    // zombie idle texture
+    Texture2D ZombieIdle = LoadTexture("textures/Idle3.png");
     AnimData ZombieIdleData{
-            {0, 0, ZombieIdle.width/6, ZombieIdle.height},
-            {WindowDimension[0]/2 - ZombieIdleData.Rec.width/2, WindowDimension[1] - ZombieIdleData.Rec.height,}, 
-            0, 
-            1.0/12.0, 
+            {0, 0, ZombieIdle.width/7, ZombieIdle.height},
+            {WindowDimension[0]/2 - ZombieIdleData.Rec.width/2, WindowDimension[1] - ZombieIdleData.Rec.height},
+            0,
+            1.0/12.0,
+            0
+    };
+
+    // zombie walk texture
+    Texture2D ZombieWalk = LoadTexture("textures/Walk3.png");
+    const float FrameWidth = ZombieWalk.width/12;
+    AnimData ZombieWalkData{
+            {0, 0, FrameWidth, ZombieWalk.height},
+            {WindowDimension[0]/2 - ZombieWalkData.Rec.width/2, WindowDimension[1] - ZombieWalkData.Rec.height},
+            0,
+            1.0/24.0,
             0.0
     };
-
-    // zombie walking
-    Texture2D ZombieWalk = LoadTexture("texture/Walk.png");
-    AnimData ZombieWalkData{
-        {0, 0, ZombieWalk.width/6, ZombieWalk.height},
-        {}
-
-    };
+    
 
     SetTargetFPS(60);
     while(!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(WHITE);
         
-        // delta time
-        const float DeltaTime = GetFrameTime();
+       BeginDrawing();
+       ClearBackground(WHITE);
+    
 
-        // ıdle
+       // delta time 
+       const float DeltaTime = GetFrameTime();
+       bool IsWalking = false;
+
+       // ıdle animation
+       if(!IsWalking)
+       {
+        ZombieIdleData.Pos.x = ZombieWalkData.Pos.x; 
         ZombieIdleData.MoveTime = ZombieIdleData.MoveTime + DeltaTime;
-        if(ZombieIdleData.MoveTime >= ZombieIdleData.UpdateTime && !IsWalking)
+        if(ZombieIdleData.MoveTime >= ZombieIdleData.UpdateTime)
         {
             ZombieIdleData.MoveTime = 0.0;
-            // update animation fram
+            // update animation frame
             ZombieIdleData.Rec.x = ZombieIdleData.Frame * ZombieIdleData.Rec.width;
             ZombieIdleData.Frame = ZombieIdleData.Frame + 1;
-            if(ZombieIdleData.Frame > 5)
+            if(ZombieIdleData.Frame >6)
             {
                 ZombieIdleData.Frame = 0;
             }
-
         }
+        
+       }
+       
+       // walk animation
+       if(IsKeyDown(KEY_D))
+       {    
+            IsWalking = true;
+            ZombieWalkData.Rec.width = FrameWidth;
+            ZombieWalkData.Pos.x = ZombieWalkData.Pos.x + (Velocity * DeltaTime);
+            ZombieWalkData.MoveTime = ZombieWalkData.MoveTime + DeltaTime;
+            if(ZombieWalkData.MoveTime >= ZombieWalkData.UpdateTime && IsWalking == true)
+            {
+                ZombieWalkData.MoveTime = 0;
+                ZombieWalkData.Rec.x = ZombieWalkData.Frame * ZombieWalkData.Rec.width;
+                ZombieWalkData.Frame = ZombieWalkData.Frame + 1;
+                if(ZombieWalkData.Frame > 11)
+                {
+                    ZombieWalkData.Frame = 0;
+                }
 
-        // walking
-        
+            }
+            
+       }
 
-        
-        DrawTextureRec(ZombieIdle, ZombieIdleData.Rec, ZombieIdleData.Pos, WHITE);
-        
-        EndDrawing();
+       if(IsKeyDown(KEY_A))
+       {    
+            IsWalking = true;
+            ZombieWalkData.Rec.width = -FrameWidth;
+            ZombieWalkData.Pos.x = ZombieWalkData.Pos.x + (-Velocity * DeltaTime);
+            ZombieWalkData.MoveTime = ZombieWalkData.MoveTime + DeltaTime;
+            if(ZombieWalkData.MoveTime >= ZombieWalkData.UpdateTime && IsWalking == true)
+            {   
+                
+                ZombieWalkData.MoveTime = 0;
+                ZombieWalkData.Rec.x = ZombieWalkData.Frame * ZombieWalkData.Rec.width;
+                ZombieWalkData.Frame = ZombieWalkData.Frame - 1;
+                if(ZombieWalkData.Frame > 11)
+                {
+                    ZombieWalkData.Frame = 0;
+                }
+
+            }
+            
+       }
+       if(!IsWalking)
+       {
+            DrawTextureRec(ZombieIdle, ZombieIdleData.Rec, ZombieIdleData.Pos, WHITE);
+       }
+       else
+       {
+            DrawTextureRec(ZombieWalk, ZombieWalkData.Rec, ZombieWalkData.Pos, WHITE);
+       }
+
+      
+       EndDrawing();
     }
     UnloadTexture(ZombieIdle);
+    UnloadTexture(ZombieWalk);
+    
     CloseWindow();
 }
